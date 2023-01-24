cd49b7b2eca01dfde70f84bd2ababd0b90c16f8a
suraj.be <suraj.be@media.net>
2023-01-24 09:37:26 +0530

    added workout model

---
+++ models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  reps: {
+    type: Number,
+    required: true
+  },
+  load: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true /* automatically adds a createdAt property for the document  */
+})
+
+module.exports = mongoose.model('Workout', workoutSchema)
